07237960f685ed0cd2f5396de03eed082e493b0d
bbq <bbq@bbqdeMac-Pro.local>
2015-07-08 20:08:10 +0800

    Initial Commit

---
+++ XW新浪微博/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XW新浪微博
+//
+//  Created by bbq on 15/7/8.
+//  Copyright (c) 2015年 bbq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XW新浪微博/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XW新浪微博
+//
+//  Created by bbq on 15/7/8.
+//  Copyright (c) 2015年 bbq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
